980d0418c59a513ef5182f0b0c6be7af15f4ed92
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2024-03-23 01:59:50 +0530

    Refactor and clean up

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
- document.addEventListener('DOMContentLoaded', function(){
+//To prevent functions running before DOM loads
+document.addEventListener('DOMContentLoaded', function(){ 
     let playerMarker = 'O'
     let computerMarker = 'X'
+    let currentPlayer = 'player'
     const buttons = document.querySelectorAll('.button')
 
-    currentPlayer = 'player'
-
+// Displays markers when a cell is clicked on by player
     function gameBoardDisplay(){
         buttons.forEach(button => {
             console.log('hii')   
             button.addEventListener('click', () =>{
-                if (button.textContent.trim() === ''){
+                if (button.textContent === ''){
                     button.textContent = playerMarker
                 }
             })
         });
     }
 
+//Starts game and sets up board display 
     function startGame(){
         gameBoardDisplay()
     }
