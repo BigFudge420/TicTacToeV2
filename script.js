77f73d5e6592cbbbabafc7ab2f7ef79cfc1c0c47
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2024-03-23 21:59:35 +0530

    Write the code using IIFE, check for tie

--- script.js
+++ script.js
@@ -1,94 +1,177 @@
 //To prevent functions running before DOM loads
 document.addEventListener('DOMContentLoaded', function(){ 
     let MarkerBtns = document.querySelectorAll('.marker-button')
-    let playerMarker = 'X'
-    let computerMarker = 'O'
-    let gameOver = false
-    let currentPlayer = 'player'
     const buttons = document.querySelectorAll('.cell-button')
+    let board = ['','','','','','','','','']        
+    let gameOver = false
+    let humanMarker = 'X'
+    let computerMarker = 'O'
 
-// Displays markers when a cell is clicked on by player
-    function playerSelectAndDisplayCell(){
-        let playerCell
-        buttons.forEach(button => {
-            console.log('hii')   
-            button.addEventListener('click', () =>{
-                playerCell = button.getAttribute('data-field')
-                if (button.textContent === '' && (board[playerCell] === '' && !gameOver)){
-                    button.textContent = playerMarker
-                    board[playerCell] = playerMarker
-                    if (checkWin(playerMarker)){
-                        console.log('PLAYER WINS')
-                        gameOver = true
-                    }
-                    setTimeout(computerSelectAndDisplayCell(), 1000)
-                }
-                else{
-                    return
-                }
-            })
-        });
-    }
+    // Gameboard contains methods to modify the board
+    const Gameboard = (() => { 
 
-let board = ['','','','','','','','','']
-
-// Select computer choice and display marker
-function computerSelectAndDisplayCell(){
-    let computerCell = Math.floor(Math.random() * 9)
-    let cellDOM = document.getElementById(`${computerCell}`)
-    if((board[computerCell] === '' && !gameOver) && cellDOM.textContent === ''){
-        board[computerCell] = computerMarker
-        cellDOM.textContent = computerMarker
-        console.log(board)
-        if (checkWin(computerMarker)){
-            console.log('COMPUTER WINS')
-            gameOver = true
-        }
-    }
-    else if (!gameOver){
-        computerSelectAndDisplayCell()
-    }
-}
-
-// Sets markers
-function setMarker(){
-    MarkerBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            console.log('hellloo')
-            playerMarker = btn.textContent
-            computerMarker = playerMarker === 'X' ? 'O' : 'X';
-        }) 
-    })
-}
-
-// To check win
-function checkWin(marker){
-    let winningCombinations = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
-    return winningCombinations.some((combination) => {
-        return combination.every((index) => {
-            if (board[index] === marker){
+        // Updates board 
+        const updateBoard = (index, marker) => {
+            if (index >= 0 && board[index] === '' && board.length > index && !gameOver){
+                board[index] = marker
                 return true
             }
-        })
-    })
-}
+            else {
+                return false
+            }
+        }
 
-//Starts game and sets up board display 
-    function startGame(){
-        playerSelectAndDisplayCell()
-        setMarker()
-    }
-    
-    startGame()
+        // Resets board 
+        const resetBoard = () => {
+            board = ['','','','','','','','','']
+        }
+
+        // Returns the methods 
+        return {updateBoard, resetBoard}
+    })()
+
+    //Gamecontroller controls gameflow 
+    const Gamecontroller = (() => {
+        gameOver = false
+
+        //Checks if win conditions are met
+        const checkWin = (marker) => {
+
+            //All possible win combinations
+            let winningCombinations = [
+                [0,1,2],
+                [3,4,5],
+                [6,7,8],
+                [0,3,6],
+                [1,4,7],
+                [2,5,8],
+                [0,4,8],
+                [2,4,6]
+            ]
+
+            //Returns a boolean value if any win condition is met
+            return winningCombinations.some((combination) => {
+                return combination.every((index) => {
+                    if (board[index] === marker){
+                        return true
+                    }
+                })
+            })
+        }
+        
+        // Controls the computer choice and turn
+        const computerPlay = () => {
+            let computerCell
+            let cellDOM
+            let play = false
+
+            // Selects and checks if selected cell is empty
+            do{
+                computerCell = Math.floor(Math.random() * 9)
+                cellDOM = document.getElementById(`${computerCell}`)
+                if (board[computerCell] === '' && cellDOM.textContent === ''){
+                    play = true
+                }                
+            }
+            while(!play)
+
+            // Updates board and display, and checks win for computer
+            if (play && !gameOver){
+                Gameboard.updateBoard(computerCell, computerMarker)
+                GameboardDisplay.updateBoardDisplay(computerCell, computerMarker)
+
+                // Checks for tie or win
+                if (checkWin(computerMarker)){
+                    console.log('COMPUTER WINS')
+                    gameOver = true
+                }
+                else if(board.every((cell) => cell !== '')){
+                    console.log("IT'S A TIE")
+                    gameOver = true
+                }
+            }
+        }
+
+        // Controls the human turn and round
+        const humanPlay = (e) => {
+
+            // Gets data-field of any target of an event
+            let humanCell = e.target.getAttribute('data-field')
+
+            // Updates textcontent and board when clicked on a cell
+            if (e.target.textContent === '' && (board[humanCell] === '' && !gameOver)){
+                GameboardDisplay.updateBoardDisplay(humanCell, humanMarker)
+                Gameboard.updateBoard(humanCell, humanMarker)
+                console.log('HIII')
+
+                // Checks for tie or win
+                if (checkWin(humanMarker)){
+                    console.log('PLAYER WINS')
+                    gameOver = true
+                }
+                else if(board.every((cell) => cell !== '')){
+                    console.log("IT'S A TIE")
+                    gameOver = true
+                }
+                else{
+                    setTimeout(computerPlay(), 1000)
+                }
+            }
+            else{
+                return
+            }
+        }
+
+        // Starts the game
+        const start = () => {
+            GameboardDisplay.setMarker()
+            buttons.forEach(btn => {
+                btn.addEventListener('click', humanPlay)
+            })
+        }
+
+        // Resets the game
+        const reset = () => {
+            GameboardDisplay.clearBoardDisplay()
+            Gameboard.resetBoard()
+            gameOver = false
+        }
+
+        return {start, reset, humanPlay, computerPlay, checkWin}
+
+    })()
+
+    // Board display related stuff
+    const GameboardDisplay = (() => {
+
+        // Clears the board from markers
+        const clearBoardDisplay = () => {
+            buttons.forEach(btn => {
+                btn.textContent = ''
+            })
+        }
+
+        // Updates the cell text
+        const updateBoardDisplay = (index, marker) => {
+            let button = document.getElementById(index) 
+            button.textContent = marker
+        }
+
+        // Sets markers
+        const setMarker = () => {
+            MarkerBtns.forEach(btn => {
+                btn.addEventListener('click', () => {
+                    humanMarker = btn.textContent
+                    computerMarker = humanMarker === 'X' ? 'O' : 'X';
+                })
+            })
+        }
+
+        return {clearBoardDisplay, updateBoardDisplay, setMarker}
+
+    })()
+
+    Gamecontroller.start()
 })
 
 
